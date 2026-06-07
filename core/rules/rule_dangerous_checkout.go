097bbc963b6604080638eb77db3835aaa0adc873
Aaron Dewes <aaron@nirvati.org>
2026-06-07 13:49:33 +0200

    fix: Explicitly support ${{ github.event.repository.default_branch }}

--- core/rules/rule_dangerous_checkout.go
+++ core/rules/rule_dangerous_checkout.go
@@ -63,7 +63,7 @@ func (rule *RuleDangerousCheckout) checkCheckoutAction(action *actionlint.ExecAc
 		// basicRegExp := regexp.MustCompile(`github.event.pull_request`)
 		ref := action.Inputs["ref"]
 
-		if ref != nil && !common.StaticRefRegexp.MatchString(ref.Value.Value) {
+		if ref != nil && !common.StaticRefRegexp.MatchString(ref.Value.Value) && ref.Value.Value != "${{ github.event.repository.default_branch }}" {
 			rule.Errorf(
 				ref.Value.Pos,
 				"Use of 'actions/checkout' with a custom ref.",
